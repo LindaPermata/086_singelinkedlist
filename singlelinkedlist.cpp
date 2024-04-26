8976a58ef349376cc7725da33eb2acc1f5f8884e
LindaPermata <permatalinda531@gmail.com>
2024-04-26 13:51:06 +0700

    menambahkan while

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -20,20 +20,34 @@ void addNode() {
     nodeBaru->noMhs = nim;
     nodeBaru->name = nama;
 
-    if (START == NULL || nim <= START->noMhs) {
-        if (START != NULL && nim == START->noMhs) 
+   if (START == NULL || nim <= START->noMhs) {
+        if (START != NULL && nim == START->noMhs)
         {
             cout << "NIM sudah ada" << endl;
             return;
-
         }
 
-        Node* previous = START;
-        node* current = START;
-
-        
-
-        }
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
     }
+
+    Node* previous = START;
+    Node* current = START;
+
+    while ((current != NULL) && (nim >= current->noMhs))
+    {
+        if (nim == current->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
+}
+    
 
 
