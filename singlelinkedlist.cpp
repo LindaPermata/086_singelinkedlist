e02df4a8a860f8526a01ce64ad34a6d6372c56b2
LindaPermata <permatalinda531@gmail.com>
2024-04-26 14:25:49 +0700

    membuat int pilihan

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -142,7 +142,14 @@ int main() {
         try
         {
            
-            cout <<
+            cout << "1. Tambah Data" << endl;
+            cout << "2. Hapus Data" << endl;
+            cout << "3. Tampilkan Data" << endl;
+            cout << "4. Cari Data" << endl;
+            cout << "5. Keluar" << endl;
+            cout << "Pilihan: ";
+            cin >> pilihan;
+            
         }
         
     }
